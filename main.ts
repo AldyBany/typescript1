29a6b47ae279836ab1e3708d4ecafd797483c781
Aldy Bany <64736339+AldyBany@users.noreply.github.com>
2021-02-20 23:26:36 +0200

    yug

---
+++ main.ts
@@ -0,0 +1,12 @@
+const btn = document.getElementById("btn")
+const num1 = document.getElementById("num1")! as HTMLInputElement
+const num2 = document.getElementById("num2")! as HTMLInputElement
+
+
+function add(num1: number, num2: number) {
+    return num1 + num2
+}
+
+btn.addEventListener("click", function () {
+    console.log(add(num1.value, +num2.value))
+})
